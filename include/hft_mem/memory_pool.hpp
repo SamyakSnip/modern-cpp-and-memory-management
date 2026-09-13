e4392cb82533caafd1a44d2873f1ccf95299b98e
SamyakSnip <i24ma027@amhd.svnit.ac.in>
2026-09-14 01:26:53 +0530

    constructor and distructor added so we can alocate buffer and link free_list

--- include/hft_mem/memory_pool.hpp
+++ include/hft_mem/memory_pool.hpp
@@ -15,8 +15,10 @@ class MemoryPool {
         Block* next;
     };
 
-    static constexpr std::size_t BlockSize = std::max(sizeof(T), sizeof(Block));
     static constexpr std::size_t BlockAlign = std::max(alignof(T), alignof(Block));
+    static constexpr std::size_t RawBlockSize = std::max(sizeof(T), sizeof(Block));
+    static constexpr std::size_t BlockSize = ((RawBlockSize + BlockAlign - 1) / BlockAlign) * BlockAlign;
+
 
     Block* free_list_head_ = nullptr;
 
@@ -29,6 +31,47 @@ public:
     MemoryPool& operator = (const MemoryPool&) = delete;
 
     MemoryPool() = default;
+
+    // constructor : pre_allocates all memory on the heap and initializes free list
+    explicit MemoryPool(std::size_t capacity):capacity_(capacity) 
+    {
+        if(capacity == 0) return;
+
+        std::size_t total_bytes = capacity_ * BlockSize;
+        buffer_ = static_cast<std::byte*>(
+            ::operator new(total_bytes, std::align_val_t{BlockAlign})
+        );
+
+        for(std::size_t i = 0; i < capacity_; i++){
+
+            Block* current = reinterpret_cast<Block*>(buffer_ + (i* BlockSize));
+
+        if(i+1 < capacity_){
+
+            Block* next_block = reinterpret_cast<Block*>(buffer_+((i+1) * BlockSize));
+            current->next = next_block;
+
+        } else {
+            current->next = nullptr;
+        }
+        }
+
+        free_list_head_ = reinterpret_cast<Block*>(buffer_);
+    }
+
+    // distuctor: frees the entire buffer in one go
+    ~MemoryPool() {
+        if(buffer_ != nullptr) {
+            ::operator delete(buffer_, std::align_val_t{BlockAlign});
+            buffer_ = nullptr;
+            free_list_head_ = nullptr;
+            capacity_ = 0;
+        }
+    }
+
+    
+
+
 };
     
 }
